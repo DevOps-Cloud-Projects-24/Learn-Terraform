acf7eba0ffe26bd829e1c1db4ce7cf81c131a69b
Ismailsfar7 <118299197+Ismailsfar7@users.noreply.github.com>
2024-08-06 18:11:19 -0500

    Terraform basics

--- resources/ec2.tf
+++ resources/ec2.tf
@@ -1,11 +1,10 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
 resource "aws_instance" "frontend" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
-  security_groups = ["sg-019b9f84c664cc861"]
   tags = {
     name = "frontend"
   }
